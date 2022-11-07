0e969c81c5df181d3a394e863ebd27fd8480d821
Natalienovaela <natasharafaelaw@gmail.com>
2022-11-07 22:26:23 +0800

    a few of the entities

---
+++ CaRMSLibrary/src/entity/Car.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Natalienovaela
+ */
+@Entity
+public class Car implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long carId;
+    @Column(length = 7, unique = true)
+    private String licensePlateNumber;
+    private String colour;
+    @Column(nullable = false)
+    private Boolean disabled;
+
+    public Long getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (carId != null ? carId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the carId fields are not set
+        if (!(object instanceof Car)) {
+            return false;
+        }
+        Car other = (Car) object;
+        if ((this.carId == null && other.carId != null) || (this.carId != null && !this.carId.equals(other.carId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Car[ id=" + carId + " ]";
+    }
+    
+}
