8d01ae8ccf7da80daa8d61f32efa5a576800014b
Natalienovaela <natasharafaelaw@gmail.com>
2022-11-13 22:37:09 +0800

    changes

--- CaRMS-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
+++ CaRMS-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -5,6 +5,7 @@
  */
 package ejb.session.singleton;
 
+import entity.Car;
 import entity.Category;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -13,10 +14,16 @@ import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import entity.Employee;
+import entity.Model;
 import entity.Outlet;
 import entity.Partner;
+import entity.RentalRates;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import javax.persistence.Query;
 import util.enumeration.EmployeeRole;
+import util.enumeration.CarAvailabilityStatus;
 
 
 /**
@@ -52,6 +59,16 @@ public class DataInitSessionBean {
         if(em.find(Category.class, 1l) == null) {
             initialiseCategory();
         }
+        
+        if(em.find(Model.class, 1l) == null ) {
+            initialiseModel();
+        }
+        
+        if(em.find(Car.class, 1l) == null) {
+            initialiseCar();
+        }
+        
+        
     }
     
     
@@ -169,6 +186,223 @@ public class DataInitSessionBean {
         em.flush();
         category = new Category("SUV and Minivan");
         em.persist(category);
+        em.flush();
+    }
+    
+    public void initialiseModel() {
+        Query query = em.createQuery("SELECT c FROM Category c WHERE c.categoryName = :categoryName");
+        query.setParameter("categoryName", "Standard Sedan");
+        Category category = (Category) query.getSingleResult();
+        
+        Model model = new Model("Toyota", "Corolla", category);
+        em.persist(model);
+        model.setCategory(category);
+        category.getModels().add(model);
+        em.flush();
+        
+        model = new Model("Honda", "Civic", category);
+        em.persist(model);
+        model.setCategory(category);
+        category.getModels().add(model);
+        em.flush();
+        
+        model = new Model("Nissan", "Sunny", category);
+        em.persist(model);
+        model.setCategory(category);
+        category.getModels().add(model);
+        em.flush();
+        
+        query = em.createQuery("SELECT c FROM Category c WHERE c.categoryName = :categoryName");
+        query.setParameter("categoryName", "Luxury Sedan");
+        category = (Category) query.getSingleResult();
+        
+        model = new Model("Mercedes", "E class", category);
+        em.persist(model);
+        model.setCategory(category);
+        category.getModels().add(model);
+        em.flush();
+        
+        model = new Model("BMW", "5 Series", category);
+        em.persist(model);
+        model.setCategory(category);
+        category.getModels().add(model);
+        em.flush();
+        
+        model = new Model("Audi", "A6", category);
+        em.persist(model);
+        model.setCategory(category);
+        category.getModels().add(model);
+        em.flush();
+    }
+    
+    public void initialiseCar() {
+        Query query = em.createQuery("SELECT m FROM Model m WHERE m.make = :make AND m.model = :model");
+        query.setParameter("make", "Toyota");
+        query.setParameter("model", "Corolla");
+        Model model = (Model)query.getSingleResult();
+        
+        Query query2 = em.createQuery("SELECT o FROM Outlet o WHERE o.outletName = :name");
+        query2.setParameter("name", "Outlet A");
+        Outlet outletA = (Outlet)query2.getSingleResult();
+        
+        Car car = new Car("SS00A1TC", model, outletA);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        car = new Car("SS00A2TC", model, outletA);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        car = new Car("SS00A3TC", model, outletA);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        query = em.createQuery("SELECT m FROM Model m WHERE m.make = :make AND m.model = :model");
+        query.setParameter("make", "Honda");
+        query.setParameter("model", "Civic");
+        model = (Model)query.getSingleResult();
+        
+        query2 = em.createQuery("SELECT o FROM Outlet o WHERE o.outletName = :name");
+        query2.setParameter("name", "Outlet B");
+        Outlet outletB = (Outlet)query2.getSingleResult();
+        
+        car = new Car("SS00B1HC", model, outletB);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        car = new Car("SS00B2HC", model, outletB);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        car = new Car("SS00B3HC", model, outletB);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        query = em.createQuery("SELECT m FROM Model m WHERE m.make = :make AND m.model = :model");
+        query.setParameter("make", "Nissan");
+        query.setParameter("model", "Sunny");
+        model = (Model)query.getSingleResult();
+        
+        query2 = em.createQuery("SELECT o FROM Outlet o WHERE o.outletName = :name");
+        query2.setParameter("name", "Outlet C");
+        Outlet outletC = (Outlet)query2.getSingleResult();
+        
+        car = new Car("SS00C1NS", model, outletC);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        car = new Car("SS00C2NS", model, outletC);
+        car.setAvailStatus(CarAvailabilityStatus.AVAILABLE);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        car = new Car("SS00C3NS", model, outletC);
+        car.setAvailStatus(CarAvailabilityStatus.REPAIR);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        query = em.createQuery("SELECT m FROM Model m WHERE m.make = :make AND m.model = :model");
+        query.setParameter("make", "Mercedes");
+        query.setParameter("model", "E Class");
+        model = (Model)query.getSingleResult();
+        
+        car = new Car("LS00A4ME", model, outletA);
+        car.setAvailStatus(CarAvailabilityStatus.REPAIR);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        query = em.createQuery("SELECT m FROM Model m WHERE m.make = :make AND m.model = :model");
+        query.setParameter("make", "BMW");
+        query.setParameter("model", "5 Series");
+        model = (Model)query.getSingleResult();
+        
+        car = new Car("LS00B4B5", model, outletB);
+        car.setAvailStatus(CarAvailabilityStatus.REPAIR);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+        
+        query = em.createQuery("SELECT m FROM Model m WHERE m.make = :make AND m.model = :model");
+        query.setParameter("make", "Audi");
+        query.setParameter("model", "A6");
+        model = (Model)query.getSingleResult();
+        
+        car = new Car("LS00C4A6", model, outletC);
+        car.setAvailStatus(CarAvailabilityStatus.REPAIR);
+        model.getCars().add(car);
+        em.persist(car);
+        em.flush();
+    }
+    
+    public void initialiseRentalRates() throws ParseException {
+        Query query = em.createQuery("SELECT c FROM Category c WHERE c.categoryName = :categoryName");
+        query.setParameter("categoryName", "Standard Sedan");
+        Category category = (Category) query.getSingleResult();
+        
+        RentalRates rental = new RentalRates("Standard Sedan - Default", "Default", category, BigDecimal.valueOf(100), null, null);
+        em.persist(rental);
+        em.flush();
+        SimpleDateFormat date = new SimpleDateFormat("dd/mm/yyyy HH:mm");
+        
+        rental = new RentalRates("Standard Sedan - Weekend Promo", "Promotion", category, BigDecimal.valueOf(80), date.parse("09/12/2022 12:00"), date.parse("11/12/2022 00:00"));
+        em.persist(rental);
+        em.flush();
+        
+        query = em.createQuery("SELECT c FROM Category c WHERE c.categoryName = :categoryName");
+        query.setParameter("categoryName", "Family Sedan");
+        category = (Category) query.getSingleResult();
+        
+        rental = new RentalRates("Family Sedan - Default", "Default", category, BigDecimal.valueOf(200), null, null);
+        em.persist(rental);
+        em.flush();
+        
+        query = em.createQuery("SELECT c FROM Category c WHERE c.categoryName = :categoryName");
+        query.setParameter("categoryName", "Luxury Sedan");
+        category = (Category) query.getSingleResult();
+        
+        rental = new RentalRates("Luxury Sedan - Default", "Default", category, BigDecimal.valueOf(300), null, null);
+        em.persist(rental);
+        em.flush();
+        
+        rental = new RentalRates("Luxury Sedan - Monday", "Peak",category, BigDecimal.valueOf(310), date.parse("05/12/2022 00:00"), date.parse("05/12/2022 23:59"));
+        em.persist(rental);
+        em.flush();
+        
+        rental = new RentalRates("Luxury Sedan - Tuesday", "Peak",category, BigDecimal.valueOf(320), date.parse("06/12/2022 00:00"), date.parse("06/12/2022 23:59"));
+        em.persist(rental);
+        em.flush();
+        
+        rental = new RentalRates("Luxury Sedan - Wednesday", "Peak",category, BigDecimal.valueOf(330), date.parse("07/12/2022 00:00"), date.parse("07/12/2022 23:59"));
+        em.persist(rental);
+        em.flush();
+        
+        rental = new RentalRates("Luxury Sedan - WeekdayPromo", "Peak" ,category, BigDecimal.valueOf(320), date.parse("07/12/2022 12:00"), date.parse("08/12/2022 12:00"));
+        em.persist(rental);
+        em.flush();
+       
+        query = em.createQuery("SELECT c FROM Category c WHERE c.categoryName = :categoryName");
+        query.setParameter("categoryName", "SUV and Minivan");
+        category = (Category) query.getSingleResult();
+        
+        rental = new RentalRates("Luxury Sedan - Default", "Default" ,category, BigDecimal.valueOf(400), null, null);
+        em.persist(rental);
         em.flush();
     }
 }
